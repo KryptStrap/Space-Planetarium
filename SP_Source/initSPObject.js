a416d56c1239673ddb7880020eb0e4d7b286cc70
KryptStrap <k.komarov31.01.06@gmail.com>
2025-02-12 15:38:30 +0300

    Added rotation system around the coordinate center of the parent object.

--- SP_Source/initSPObject.js
+++ SP_Source/initSPObject.js
@@ -4,21 +4,30 @@ export class spObject extends webGlObject {
     #radius;
     #parentPlanet;
     #distanceFromParent;
+    #revolution;
 
-    #moveOrbit;
-
-    constructor(gl, program, positions, color, primitives, parrentPlanet) {
-        super(gl, program, positions, color, primitives)
+    constructor(gl, program, positions, color, indices, parrentPlanet, distanceFromParent) {
+        super(gl, program, positions, color, indices);
 
         this.#parentPlanet = parrentPlanet;
-        this.#moveOrbit = false
+        this.#distanceFromParent = distanceFromParent;
+        this.#revolution = 0;
     }
 
-    moveOrbitStart() {
-        this.#moveOrbit = true;
 
-        while(this.#moveOrbit) {
 
-        }
+    moveOrbit(speed) {
+        this._translationArray[0] = this.#parentPlanet._translationArray[0] + this.#distanceFromParent * Math.sin(this.#revolution);
+        this._translationArray[2] = this.#parentPlanet._translationArray[2] + this.#distanceFromParent * Math.cos(this.#revolution);
+        this.#revolution += speed;
+
+        this._rotationArray[1] += 0.01;
+
+        glMatrix.mat4.translate(this._translationMatrix, glMatrix.mat4.create(), this._translationArray);
+        glMatrix.mat4.rotateY(this._rotationMatrix, glMatrix.mat4.create(), this._rotationArray[1]);
+        
+
+        this._gl.uniformMatrix4fv(this._u_Translation_Matrix_Location, false, this._translationMatrix);
+        this._gl.uniformMatrix4fv(this._u_Rotation_Matrix_Location, false, this._rotationMatrix);
     }
 }
