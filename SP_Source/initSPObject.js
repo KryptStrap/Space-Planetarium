665f19b4499b230f96b7f071f08cd66be0fae2ed
KryptStrap <k.komarov31.01.06@gmail.com>
2025-03-06 11:26:00 +0300

    Changing the definition of angular velocity of rotation

--- SP_Source/initSPObject.js
+++ SP_Source/initSPObject.js
@@ -1,24 +1,19 @@
-import {webGlObject} from "../Engine/initWebGlObject.js";
+import {webGlObject, webGlCamera} from "../Engine/initWebGlObject.js";
 import { fileReader, imageLoader } from "../Engine/fileLoader.js";
 
-export async function createSpacePlanetObject(gl, program, modelDataPath, imagePath, radius, parrentPlanet, distanceFromParent) {
-    const modelData = await fileReader(modelDataPath);
-    const image = await imageLoader(imagePath);
-    return new spacePlanetObject(gl, program, modelData, image, radius, parrentPlanet, distanceFromParent);
-}
-
 export class spacePlanetObject extends webGlObject {
-    #radius
+    #radius = 1;
     #parentPlanet;
-    #distanceFromParent;
-    #revolution;
+    #distanceFromParent = 0;
+    #revolution = 0;
+
+    #axisRotationSpeed = 0;
+    #orbitalRotationSpeed = 0;
 
     constructor(gl, program, modelData, image, radius) {
         super(gl, program, modelData, image);
         
         this.#radius = radius;
-        this.#parentPlanet = null;
-        this.#distanceFromParent = null;
         this.#revolution = 0;
 
         this.setScale([this.#radius, this.#radius, this.#radius]);
@@ -29,13 +24,18 @@ export class spacePlanetObject extends webGlObject {
         this.#distanceFromParent = distanceFromParent;
     }
 
-    moveOrbit(axisRotationSpeed, orbitalRotationSpeed) {
-        if(this.#parentPlanet != null) {
+    setAngularSpeedRotation(axisRotationSpeed, orbitalRotationSpeed) {
+        this.#axisRotationSpeed = axisRotationSpeed;
+        this.#orbitalRotationSpeed = orbitalRotationSpeed;
+    }
+
+    rotationStep() {
+        if(this.#parentPlanet !== undefined) {
             this._translationArray[0] = this.#parentPlanet._translationArray[0] + this.#distanceFromParent * Math.sin(this.#revolution);
             this._translationArray[2] = this.#parentPlanet._translationArray[2] + this.#distanceFromParent * Math.cos(this.#revolution);
-            this.#revolution += orbitalRotationSpeed;
+            this.#revolution += this.#orbitalRotationSpeed;
 
-            this._rotationArray[1] += axisRotationSpeed;
+            this._rotationArray[1] += this.#axisRotationSpeed;
 
             glMatrix.mat4.translate(this._translationMatrix, glMatrix.mat4.create(), this._translationArray);
             glMatrix.mat4.rotateY(this._rotationMatrix, glMatrix.mat4.create(), this._rotationArray[1]);
@@ -44,11 +44,17 @@ export class spacePlanetObject extends webGlObject {
             this._gl.uniformMatrix4fv(this._u_Translation_Matrix_Location, false, this._translationMatrix);
             this._gl.uniformMatrix4fv(this._u_Rotation_Matrix_Location, false, this._rotationMatrix);
         } else {
-            this._rotationArray[1] += axisRotationSpeed;
+            this._rotationArray[1] += this.#axisRotationSpeed;
 
             glMatrix.mat4.rotateY(this._rotationMatrix, glMatrix.mat4.create(), this._rotationArray[1]);
         
             this._gl.uniformMatrix4fv(this._u_Rotation_Matrix_Location, false, this._rotationMatrix);
         }
+    }
+
+    static async create(gl, program, modelDataPath, imagePath, radius, parrentPlanet, distanceFromParent) {
+        const modelData = await fileReader(modelDataPath);
+        const image = await imageLoader(imagePath);
+        return new spacePlanetObject(gl, program, modelData, image, radius, parrentPlanet, distanceFromParent);
     }
 }
