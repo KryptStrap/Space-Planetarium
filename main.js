da9b969e609dfb22431a344cd5e2ee581c41d9b2
KryptStrap <k.komarov31.01.06@gmail.com>
2025-03-06 11:19:41 +0300

    The method of object creation has been changed

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { createWebGlContext, createShader, createProgram } from "./Engine/initWebGlContext.js";
-import { camera, frameRender } from "./Engine/initWebGlObject.js";
-import { createSpacePlanetObject } from "./SP_Source/initSPObject.js";
+import { frameRender, webGlCamera } from "./Engine/initWebGlObject.js";
+import { spacePlanetObject} from "./SP_Source/initSPObject.js";
 
 (async function main() {
     const gl = createWebGlContext();
@@ -8,27 +8,27 @@ import { createSpacePlanetObject } from "./SP_Source/initSPObject.js";
     const fragmentShader = await createShader(gl, gl.FRAGMENT_SHADER, "Engine/Shaders/standartFragmentShader.frag");
     const program = createProgram(gl, vertexShader, fragmentShader);
 
-    camera.create(gl, program, [0.0, 4000.0, 0.0], [-3.14, 0.0, 0.0]);
+    const camera = webGlCamera.create(gl, program);
+    webGlCamera.setActiveCamera(camera);
 
-    const sun0 = await createSpacePlanetObject(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 100);
+    const sun0 = await spacePlanetObject.create(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 100);
+    sun0.setAngularSpeedRotation(0.01, 0);
 
-    const sun1 = await createSpacePlanetObject(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 25);
+    const sun1 = await spacePlanetObject.create(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 25);
     sun1.setPareentPlanet(sun0, 1200);
+    sun1.setAngularSpeedRotation(0.01, 0.01);
 
-    const sun2 = await createSpacePlanetObject(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 25);
+    const sun2 = await spacePlanetObject.create(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 25);
     sun2.setPareentPlanet(sun0, 2400);
+    sun2.setAngularSpeedRotation(0.1, 0.005);
 
-    const sun3 = await createSpacePlanetObject(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 15);
+    const sun3 = await spacePlanetObject.create(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 15);
     sun3.setPareentPlanet(sun1, 200);
+    sun3.setAngularSpeedRotation(0.1, 0.02);
 
-    const sun4 = await createSpacePlanetObject(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 15);
+    const sun4 = await spacePlanetObject.create(gl, program, "./Models/sun.obj", "./Textures/sun.jpg", 15);
     sun4.setPareentPlanet(sun1, 100);
+    sun4.setAngularSpeedRotation(0.1, 0.004);
 
-    requestAnimationFrame(timeNow => frameRender(gl, timeNow, [
-        sun0,
-        sun1,
-        sun2,
-        sun3,
-        sun4
-    ]));
+    requestAnimationFrame(timeNow => frameRender(gl, timeNow));
 })()
